18246687ac1190cbffc8d376cc968ee6517070ed
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-08-19 20:30:08 +0200

    feat: use config port

--- api/src/server.js
+++ api/src/server.js
@@ -1,20 +1,21 @@
-import express from "express";
-import bp from "body-parser";
-import morgan from "morgan";
-import crosswordRouter from "./resources/crossword/crossword.router.js";
+import express from 'express'
+import bp from 'body-parser'
+import morgan from 'morgan'
+import * as config from './config/index.js'
+import crosswordRouter from './resources/crossword/crossword.router.js'
 
-const app = express();
+const app = express()
 
-app.use(bp.urlencoded({ extended: true }));
-app.use(bp.json());
-app.use(morgan("dev"));
+app.use(bp.urlencoded({ extended: true }))
+app.use(bp.json())
+app.use(morgan('dev'))
 
-app.use("/api/crossword", crosswordRouter);
+app.use('/api/crossword', crosswordRouter)
 
-app.get("/", (req, res) => {
-  res.json({ message: "hello there" });
-});
+app.get('/', (req, res) => {
+  res.json({ message: 'hello there' })
+})
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running`);
-});
+app.listen(config.port, () => {
+  console.log(`Server is running on port: ${config.port}`)
+})
