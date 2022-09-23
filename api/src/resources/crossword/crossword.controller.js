890bc2c288be4a42e4551abb47414acb3cae34d5
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-09-23 20:53:10 +0200

    feat: add basic req validation

--- api/src/resources/crossword/crossword.controller.js
+++ api/src/resources/crossword/crossword.controller.js
@@ -1,5 +1,16 @@
-import createCrosswordLevel from "../../modules/crossword/index.js";
+import createCrosswordLevel from '../../modules/crossword/index.js'
+import { TIME_PER_LEVEL } from '../../modules/crossword/config.js'
 
 export const getLevel = (req, res) => {
-  res.send({ level: createCrosswordLevel(req.params.level) });
-};
+  const isValidReq =
+    req.params.level && req.params.level >= 0 && req.params.level <= 10
+
+  if (isValidReq) {
+    res.send({
+      level: createCrosswordLevel(req.params.level),
+      time: TIME_PER_LEVEL[req.params.level],
+    })
+  } else {
+    res.send({ message: 'invalid request' })
+  }
+}
